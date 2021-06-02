e7cae410493eb2c7cb417ba7b0984f59617f7dbc
Jaehun.Kim <rlawogns0411@naver.com>
2021-06-02 20:42:04 +0900

    practice 26

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default class App extends React.Component {
+    constructor(props) {
+       super(props);
+
+       this.state = {
+         cities: [],
+       };
+    }
+    componentDidMount() {
+    fetch('https://raw.githubusercontent.com/example0312/weather-crawler/master/availableCityNames')
+      .then(response => response.json())
+      .then(cities => {
+           console.log('cities =', cities.length);
+           this.setState({
+              cities
+           });
+      });
+    }
+
+    renderItem(city) {
+      return (
+        <View style={styles.item}>
+          <Text style={styles.text}>{city}</Text>
+        </View>
+      );
+    }
+    render() {
+      return (
+        <SafeAreaView style={styles.container}>
+           <FlatList style={styles.container}
+                 keyExtractor={item => item}
+                 renderItem={({ item }) => this.renderItem(item)}
+                 data={this.state.cities}
+           />
+           <StatusBar style="auto" />
+        </SafeAreaView>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+      flex: 1,
   },
+  item: {
+      flex: 1,
+      height: 50,
+      justifyContent: 'center',
+      borderWidth: 1,
+      borderColor: 'orange',
+    },
+    text: {
+      fontSize: 20,
+      textAlign: 'center',
+    },
 });
