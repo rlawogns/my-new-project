2fdab45d7dc19e6eee52750c9ad8d569f27eecf6
Jaehun.Kim <rlawogns0411@naver.com>
2021-06-02 22:15:15 +0900

    practice 25 + data add

--- App.js
+++ App.js
@@ -1,62 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-
+// after `expo install react-native-safe-area-context`.
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 export default class App extends React.Component {
-    constructor(props) {
-       super(props);
 
-       this.state = {
-         cities: [],
-       };
-    }
-    componentDidMount() {
+  componentDidMount() {
     fetch('https://raw.githubusercontent.com/example0312/weather-crawler/master/availableCityNames')
       .then(response => response.json())
-      .then(cities => {
-           console.log('cities =', cities.length);
-           this.setState({
-              cities
-           });
-      });
-    }
-
-    renderItem(city) {
-      return (
-        <View style={styles.item}>
-          <Text style={styles.text}>{city}</Text>
-        </View>
-      );
-    }
-    render() {
-      return (
-        <SafeAreaView style={styles.container}>
-           <FlatList style={styles.container}
-                 keyExtractor={item => item}
-                 renderItem={({ item }) => this.renderItem(item)}
-                 data={this.state.cities}
-           />
-           <StatusBar style="auto" />
-        </SafeAreaView>
-      );
-    }
+      .then(console.log);
+  }
+  renderItem({ name }) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.text}>{name}</Text>
+      </View>
+    );
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList style={styles.container}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => this.renderItem(item)}
+          data={this.fruits}
+        />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
-  container:{
-      flex: 1,
+  container: {
+    flex: 1,
   },
   item: {
-      flex: 1,
-      height: 50,
-      justifyContent: 'center',
-      borderWidth: 1,
-      borderColor: 'orange',
-    },
-    text: {
-      fontSize: 20,
-      textAlign: 'center',
-    },
+    flex: 1,
+    height: 50,
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'orange',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
 });
