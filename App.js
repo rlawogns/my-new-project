a749d948c2eafd239289a830524e41edc548ffad
Jaehun.Kim <rlawogns0411@naver.com>
2021-06-02 22:38:38 +0900

    data update2

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default class App extends React.Component {
 
   componentDidMount() {
-    fetch('https://raw.githubusercontent.com/rlawogns/realcoding_App/CityName/availableCityNames')
+    fetch('https://raw.githubusercontent.com/rlawogns/realcoding_App/main/CityName/availableCityNames.html')
       .then(response => response.json())
       .then(console.log);
   }
